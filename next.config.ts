e8a85c81556e94fd1a6b6871076b070f8a4e22aa
aniket aggarwal <aniket16july@gmail.com>
2025-12-30 19:22:48 +0530

    fix: force webpack build via vercel.json and clean config

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,7 @@ const withPWA = withPWAInit({
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
